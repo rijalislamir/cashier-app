ee6ff22c582b81b55fc702095da3de3f15996e31
rijalislamir <rijalislamir@gmail.com>
2022-06-30 22:41:28 +0700

    Fix order and add 2 buttons ('.' and 'OK')

--- src/components/Numpad.jsx
+++ src/components/Numpad.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const Numpad = () => {
     return (
         <div className="numpad">
-            <button className="numpad__item">1</button>
-            <button className="numpad__item">2</button>
-            <button className="numpad__item">3</button>
-            <button className="numpad__item">4</button>
-            <button className="numpad__item">5</button>
-            <button className="numpad__item">6</button>
             <button className="numpad__item">7</button>
             <button className="numpad__item">8</button>
             <button className="numpad__item">9</button>
+            <button className="numpad__item">4</button>
+            <button className="numpad__item">5</button>
+            <button className="numpad__item">6</button>
+            <button className="numpad__item">1</button>
+            <button className="numpad__item">2</button>
+            <button className="numpad__item">3</button>
+            <button className="numpad__item">.</button>
             <button className="numpad__item">0</button>
+            <button className="numpad__item">OK</button>
         </div>
     );
 }
