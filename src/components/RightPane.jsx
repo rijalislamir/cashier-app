f8f5d6f251bfa7a0fa4df17a596700626c16b8b6
rijalislamir <rijalislamir@gmail.com>
2022-07-01 11:41:56 +0700

    Add new button

--- src/components/RightPane.jsx
+++ src/components/RightPane.jsx
@@ -8,6 +8,7 @@ const RightPane = () => {
             <ProductListItem />
             <ProductListItem />
             <ProductListItem />
+            <button className="new-product-button">+</button>
         </section>
     );
 }
